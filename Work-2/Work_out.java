535bbe662005ff25ff544e6441d2c9478586dd44
Роман Чечелев <rd.chechelev@mail.ru>
2026-06-16 14:22:31 +0300

    work_5

--- Work-2/Work_out.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Work_output {
-    public static void main() {
-        System.out.println("please,write your name:");
-        Scanner scan = new Scanner(System.in);
-        String name = scan.next();
-        System.out.println("Hello " +name+ ", nice to meet you!");
-    }
-}
